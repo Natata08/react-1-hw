775f4b8eb71f3fee3d9db2e12151da0872f0df2a
Natalia Lapina <lapinann08@gmail.com>
2024-08-28 13:57:23 +0200

    add styling to OurValues

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -1,19 +1,10 @@
-import styles from './page.module.css';
+import styles from "./page.module.css";
+import OurValues from "./OurValues";
 
 // TASK - React 1 week 1
 // After you are finished with creating the page, move the OurValues, OurCrew, OurPartners components into their own files
 // OurValues.js, OurCrew.js, OurPartners.js should live in this folder
 // import and use the components from the newly created files
-
-const OurValues = () => {
-  // TASK - React 1 week 1
-  // Create the "Our Values" section
-  // Use the descriptions provided in /app/about_us/README.md
-  // Some inspiration ideas found in /data/inspiration_about_us
-  return (
-    <p> ADD OUR VALUES HERE </p>
-  );
-};
 
 const OurCrew = () => {
   // TASK - React 1 week 1
@@ -21,10 +12,8 @@ const OurCrew = () => {
   // Use the descriptions provided in /app/about_us/README.md
   // Use the pictures from /public/crew
   // Some inspiration ideas found in /data/inspiration_about_us
-  return (
-    <p> ADD OUR CREW HERE </p>
-  );
-}
+  return <p> ADD OUR CREW HERE </p>;
+};
 
 const OurPartners = () => {
   // TASK - React 1 week 1
@@ -32,31 +21,28 @@ const OurPartners = () => {
   // Use the descriptions provided in /app/about_us/README.md
   // Use the pictures from /public/crew
   // Some inspiration ideas found in /data/inspiration_about_us
-  return (
-    <p> ADD OUR Partners HERE </p>
-  );
-}
-
+  return <p> ADD OUR Partners HERE </p>;
+};
 
 export const Crew = () => {
   return (
-    <div className="fullBGpicture">
-      <main className="mainContent">
+    <div className='fullBGpicture'>
+      <main className='mainContent'>
         <h1>About us</h1>
-        <section className="card">
-          <h2>Our Values</h2>
-          <OurValues/>
+        <section className='card'>
+          <h2 className={styles["card-title"]}>Our Values</h2>
+          <OurValues />
         </section>
-        <section className="card">
+        <section className='card'>
           <h2>The crew</h2>
-          <OurCrew/>
+          <OurCrew />
         </section>
 
-         {/* TASK - React 1 week 1 */}
-         {/* Add in the "OurPartners" component here */}
+        {/* TASK - React 1 week 1 */}
+        {/* Add in the "OurPartners" component here */}
       </main>
     </div>
   );
-}
+};
 
 export default Crew;
